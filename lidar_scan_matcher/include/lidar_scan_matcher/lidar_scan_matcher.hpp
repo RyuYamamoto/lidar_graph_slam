13eabd3b328827f4f7bf6599217cac27223b76cf
RyuYamamoto <ryu.yamamoto@tier4.jp>
2023-01-07 13:35:51 +0900

    feat: add ndt_omp

--- lidar_scan_matcher/include/lidar_scan_matcher/lidar_scan_matcher.hpp
+++ lidar_scan_matcher/include/lidar_scan_matcher/lidar_scan_matcher.hpp
@@ -7,15 +7,23 @@
 #include <nav_msgs/msg/path.hpp>
 #include <sensor_msgs/msg/imu.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+
+#include <tf2_eigen/tf2_eigen.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+#include <tf2/transform_datatypes.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pclomp/ndt_omp.h>
 
+using PointType = pcl::PointXYZ;
+
 class LidarScanMatcher : public rclcpp::Node
 {
-  using PointType = pcl::PointXYZ;
-
 public:
   LidarScanMatcher();
   ~LidarScanMatcher() = default;
@@ -27,9 +35,9 @@ public:
   pcl::Registration<PointType, PointType>::Ptr get_registration();
   nav_msgs::msg::Odometry convert_to_odometry(const geometry_msgs::msg::Pose pose);
 
-  pcl::PointCloud<PointType> transform_point_cloud(
-    const std::string source_frame, const std::string target_frame,
-    pcl::PointCloud<PointType>::Ptr cloud);
+  pcl::PointCloud<PointType>::Ptr transform_point_cloud(
+    const std::string source_frame, const std::string target_frame, const rclcpp::Time stamp,
+    pcl::PointCloud<PointType>::Ptr input_cloud_ptr);
 
 private:
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sensor_points_subscriber_;
@@ -43,6 +51,10 @@ private:
   geometry_msgs::msg::Pose current_pose_;
 
   std::deque<sensor_msgs::msg::Imu> imu_queue_;
+
+  tf2_ros::Buffer tf_buffer_{ get_clock() };
+  tf2_ros::TransformListener tf_listener_{ tf_buffer_ };
+  std::shared_ptr<tf2_ros::TransformBroadcaster> broadcaster_;
 };
 
 #endif
--- lidar_scan_matcher/src/lidar_scan_matcher.cpp
+++ lidar_scan_matcher/src/lidar_scan_matcher.cpp
@@ -2,10 +2,41 @@
 
 LidarScanMatcher::LidarScanMatcher() : Node("lidar_scan_matcher_node")
 {
-
+  sensor_points_subscriber_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+    "points_raw", rclcpp::SensorDataQoS().keep_last(1),
+    std::bind(&LidarScanMatcher::callback_cloud, this, std::placeholders::_1));
 }
 
 void LidarScanMatcher::callback_cloud(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
 {
   pcl::PointCloud<PointType>::Ptr input_cloud_ptr(new pcl::PointCloud<PointType>);
+}
+
+pcl::PointCloud<PointType>::Ptr LidarScanMatcher::transform_point_cloud(
+  const std::string source_frame, const std::string target_frame, const rclcpp::Time stamp,
+  pcl::PointCloud<PointType>::Ptr input_cloud_ptr)
+{
+  pcl::PointCloud<PointType>::Ptr transform_cloud_ptr(new pcl::PointCloud<PointType>);
+  geometry_msgs::msg::TransformStamped frame_transform;
+  try {
+    frame_transform =
+      tf_buffer_.lookupTransform(source_frame, target_frame, stamp, tf2::durationFromSec(0.5));
+  } catch (tf2::TransformException & ex) {
+    RCLCPP_ERROR(get_logger(), "%s", ex.what());
+    frame_transform.header.stamp = stamp;
+    frame_transform.header.frame_id = source_frame;
+    frame_transform.child_frame_id = target_frame;
+    frame_transform.transform.translation.x = 0.0;
+    frame_transform.transform.translation.y = 0.0;
+    frame_transform.transform.translation.z = 0.0;
+    frame_transform.transform.rotation.w = 1.0;
+    frame_transform.transform.rotation.x = 0.0;
+    frame_transform.transform.rotation.y = 0.0;
+    frame_transform.transform.rotation.z = 0.0;
+  }
+  const Eigen::Affine3d frame_affine = tf2::transformToEigen(frame_transform);
+  const Eigen::Matrix4f frame_matrix = frame_affine.matrix().cast<float>();
+  pcl::transformPointCloud(*input_cloud_ptr, *transform_cloud_ptr, frame_matrix);
+
+  return transform_cloud_ptr;
 }
