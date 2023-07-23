76660e87cc5aa9f963d9eb279691c2ff42ef192d
RyuYamamoto <ryu.yamamoto@tier4.jp>
2023-07-23 22:43:31 +0900

    feat: add slam viewer

---
+++ lidar_graph_slam_viewer/include/lidar_graph_slam_viewer/lidar_graph_slam_viewer.hpp
@@ -0,0 +1,112 @@
+#ifndef _LIDAR_GRAPH_SLAM_VIEWER_HPP_
+#define _LIDAR_GRAPH_SLAM_VIEWER_HPP_
+
+#include <Eigen/Dense>
+#include <glk/effects/screen_space_lighting.hpp>
+#include <glk/lines.hpp>
+#include <glk/pointcloud_buffer.hpp>
+#include <glk/pointcloud_buffer_pcl.hpp>
+#include <glk/primitives/primitives.hpp>
+#include <glk/thin_lines.hpp>
+#include <guik/viewer/light_viewer.hpp>
+#include <pcl_ros/transforms.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rosbag2_interfaces/srv/set_rate.hpp>
+#include <rosbag2_interfaces/srv/toggle_paused.hpp>
+#include <tf2_eigen/tf2_eigen.hpp>
+
+#include "lidar_graph_slam_msgs/msg/key_frame_array.hpp"
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <geometry_msgs/msg/twist_with_covariance_stamped.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
+#include <fmt/core.h>
+#include <implot.h>
+#include <pcl/common/common.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <portable-file-dialogs.h>
+#include <tf2/convert.h>
+#include <tf2/transform_datatypes.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+
+struct PlotBuffer
+{
+  ImVector<ImVec2> data;
+  int max_queue_size;
+  int offset;
+
+  PlotBuffer() { max_queue_size = 2000; }
+  void add(float x, float y)
+  {
+    if (data.size() < max_queue_size) {
+      data.push_back(ImVec2(x, y));
+    } else {
+      data[offset] = ImVec2(x, y);
+      offset = (offset + 1) % max_queue_size;
+    }
+  }
+};
+
+class LidarGraphSlamViewer : public rclcpp::Node
+{
+public:
+  LidarGraphSlamViewer(const rclcpp::NodeOptions & node_options);
+  ~LidarGraphSlamViewer();
+
+private:
+  void sensor_points_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+  void map_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+  void key_frame_callback(const lidar_graph_slam_msgs::msg::KeyFrameArray::SharedPtr msg);
+  void candidate_path_callback(const nav_msgs::msg::Path::SharedPtr msg);
+
+  void create_viewer();
+  void viewer_thread();
+
+  geometry_msgs::msg::TransformStamped get_transform(
+    const std::string source_frame, const std::string target_frame);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr transform_point_cloud(
+    const pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud_ptr,
+    const geometry_msgs::msg::TransformStamped transform);
+
+private:
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr map_subscriber_;
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sensor_points_subscriber_;
+  rclcpp::Subscription<lidar_graph_slam_msgs::msg::KeyFrameArray>::SharedPtr
+    key_frame_array_subscriber_;
+  rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr candidate_path_subscriber_;
+
+  rclcpp::Client<rosbag2_interfaces::srv::TogglePaused>::SharedPtr toggle_client_;
+  rclcpp::Client<rosbag2_interfaces::srv::SetRate>::SharedPtr rate_client_;
+
+  tf2_ros::Buffer tf_buffer_{get_clock()};
+  tf2_ros::TransformListener tf_listener_{tf_buffer_};
+
+  Eigen::Matrix4f latest_pose_matrix_;
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr latest_sensor_points_;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr map_;
+
+  nav_msgs::msg::Path candidate_path_;
+  lidar_graph_slam_msgs::msg::KeyFrameArray key_frame_;
+  std::vector<Eigen::Vector3f> key_frame_line_;
+
+  std::thread viewer_thread_;
+
+  float play_rate_{1.0f};
+
+  bool is_candidate_key_frame_plot_{false};
+  bool is_key_frame_plot_{false};
+  bool is_map_loaded_{false};
+  bool is_first_map_plot_{false};
+};
+
+#endif
---
+++ lidar_graph_slam_viewer/src/lidar_graph_slam_viewer.cpp
@@ -0,0 +1,205 @@
+#include "lidar_graph_slam_viewer/lidar_graph_slam_viewer.hpp"
+
+LidarGraphSlamViewer::LidarGraphSlamViewer(const rclcpp::NodeOptions & node_options)
+: Node("lidar_graph_slam_viewer", node_options)
+{
+  map_subscriber_ = create_subscription<sensor_msgs::msg::PointCloud2>(
+    "points_map", rclcpp::QoS(1).transient_local(),
+    std::bind(&LidarGraphSlamViewer::map_callback, this, std::placeholders::_1));
+  sensor_points_subscriber_ = create_subscription<sensor_msgs::msg::PointCloud2>(
+    "points_raw", rclcpp::SensorDataQoS().keep_last(1),
+    std::bind(&LidarGraphSlamViewer::sensor_points_callback, this, std::placeholders::_1));
+  key_frame_array_subscriber_ = create_subscription<lidar_graph_slam_msgs::msg::KeyFrameArray>(
+    "modified_key_frame", 5,
+    std::bind(&LidarGraphSlamViewer::key_frame_callback, this, std::placeholders::_1));
+  candidate_path_subscriber_ = create_subscription<nav_msgs::msg::Path>(
+    "candidate_key_frame", 5,
+    std::bind(&LidarGraphSlamViewer::candidate_path_callback, this, std::placeholders::_1));
+
+  toggle_client_ =
+    create_client<rosbag2_interfaces::srv::TogglePaused>("rosbag2_player/toggle_paused");
+  toggle_client_->wait_for_service(std::chrono::milliseconds(200));
+
+  rate_client_ = create_client<rosbag2_interfaces::srv::SetRate>("rosbag2_player/set_rate");
+  rate_client_->wait_for_service(std::chrono::milliseconds(200));
+
+  latest_sensor_points_.reset(new pcl::PointCloud<pcl::PointXYZ>);
+
+  create_viewer();
+}
+
+LidarGraphSlamViewer::~LidarGraphSlamViewer() { viewer_thread_.join(); }
+
+void LidarGraphSlamViewer::sensor_points_callback(
+  const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+{
+  auto map_to_base_frame = get_transform("base_link", "map");
+  const Eigen::Affine3d frame_affine = tf2::transformToEigen(map_to_base_frame);
+  latest_pose_matrix_ = frame_affine.matrix().cast<float>();
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr transform_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::fromROSMsg(*msg, *input_cloud_ptr);
+  transform_cloud_ptr = transform_point_cloud(input_cloud_ptr, map_to_base_frame);
+  latest_sensor_points_ = transform_cloud_ptr;
+}
+
+void LidarGraphSlamViewer::map_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+{
+  if (map_ == nullptr) {
+    map_.reset(new pcl::PointCloud<pcl::PointXYZ>);
+  }
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr map_raw(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr map_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::fromROSMsg(*msg, *map_);
+
+  is_map_loaded_ = true;
+  is_first_map_plot_ = true;
+}
+
+void LidarGraphSlamViewer::key_frame_callback(
+  const lidar_graph_slam_msgs::msg::KeyFrameArray::SharedPtr msg)
+{
+  key_frame_ = *msg;
+  is_key_frame_plot_ = true;
+}
+
+void LidarGraphSlamViewer::candidate_path_callback(const nav_msgs::msg::Path::SharedPtr msg)
+{
+  candidate_path_ = *msg;
+  is_candidate_key_frame_plot_ = true;
+}
+
+geometry_msgs::msg::TransformStamped LidarGraphSlamViewer::get_transform(
+  const std::string source_frame, const std::string target_frame)
+{
+  geometry_msgs::msg::TransformStamped frame_transform;
+  try {
+    frame_transform = tf_buffer_.lookupTransform(
+      target_frame, source_frame, tf2::TimePointZero, tf2::durationFromSec(0.5));
+  } catch (tf2::TransformException & ex) {
+    RCLCPP_ERROR(get_logger(), "%s", ex.what());
+    frame_transform.header.stamp = now();
+    frame_transform.header.frame_id = source_frame;
+    frame_transform.child_frame_id = target_frame;
+    frame_transform.transform.translation.x = 0.0;
+    frame_transform.transform.translation.y = 0.0;
+    frame_transform.transform.translation.z = 0.0;
+    frame_transform.transform.rotation.w = 1.0;
+    frame_transform.transform.rotation.x = 0.0;
+    frame_transform.transform.rotation.y = 0.0;
+    frame_transform.transform.rotation.z = 0.0;
+  }
+  return frame_transform;
+}
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr LidarGraphSlamViewer::transform_point_cloud(
+  const pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud_ptr,
+  const geometry_msgs::msg::TransformStamped transform)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr transform_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>);
+  const Eigen::Affine3d frame_affine = tf2::transformToEigen(transform);
+  const Eigen::Matrix4f frame_matrix = frame_affine.matrix().cast<float>();
+  pcl::transformPointCloud(*input_cloud_ptr, *transform_cloud_ptr, frame_matrix);
+
+  return transform_cloud_ptr;
+}
+
+void LidarGraphSlamViewer::create_viewer()
+{
+  RCLCPP_INFO_STREAM(get_logger(), "create viewer...");
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+  viewer_thread_ = std::thread(std::bind(&LidarGraphSlamViewer::viewer_thread, this));
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+}
+
+void LidarGraphSlamViewer::viewer_thread()
+{
+  auto viewer = guik::LightViewer::instance();
+
+  viewer->register_ui_callback("rosbag player", [&]() {
+    ImGui::Begin("rosbag player", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+
+    if (ImGui::Button("start/pause")) {
+      auto future = toggle_client_->async_send_request(
+        std::make_shared<rosbag2_interfaces::srv::TogglePaused::Request>());
+      future.wait_for(std::chrono::milliseconds(200));
+    }
+
+    if (ImGui::InputFloat("play rate", &play_rate_, 0.1f, 1.0f, "%.1f", 1)) {
+      play_rate_ = std::max(0.1f, play_rate_);
+      auto request = std::make_shared<rosbag2_interfaces::srv::SetRate::Request>();
+      request->rate = play_rate_;
+      auto future = rate_client_->async_send_request(request);
+      future.wait_for(std::chrono::milliseconds(200));
+    }
+  });
+
+  while (viewer->spin_once()) {
+    if (!is_map_loaded_) {
+      continue;
+    }
+
+    if (is_first_map_plot_) {
+      auto map_cloud_buffer = glk::create_point_cloud_buffer(*map_);
+
+      viewer->update_drawable("point_cloud_map", map_cloud_buffer, guik::Rainbow());
+
+      is_first_map_plot_ = false;
+    }
+
+    if (is_key_frame_plot_) {
+      key_frame_line_.clear();
+      int idx = 0;
+      for (auto key_frame : key_frame_.keyframes) {
+        Eigen::Vector3f vertices;
+        vertices.x() = key_frame.pose.position.x;
+        vertices.y() = key_frame.pose.position.y;
+        vertices.z() = key_frame.pose.position.z;
+        key_frame_line_.emplace_back(vertices);
+
+        Eigen::Affine3d affine;
+        tf2::fromMsg(key_frame.pose, affine);
+        viewer->update_drawable(
+          fmt::format("key_frame_{}", idx++), glk::Primitives::coordinate_system(),
+          guik::VertexColor(affine.matrix().cast<float>()));
+      }
+      auto line = std::make_shared<glk::ThinLines>(key_frame_line_, true);
+
+      line->set_line_width(2.0f);
+      viewer->update_drawable("key_frame_line", line, guik::FlatColor(0.0f, 1.0f, 0.0f, 1.0f));
+
+      is_key_frame_plot_ = false;
+    }
+
+    if (is_candidate_key_frame_plot_) {
+      std::vector<Eigen::Vector3f> candidate_line;
+      for (auto candidate : candidate_path_.poses) {
+        Eigen::Vector3f vertices;
+        vertices.x() = candidate.pose.position.x;
+        vertices.y() = candidate.pose.position.y;
+        vertices.z() = candidate.pose.position.z;
+        candidate_line.emplace_back(vertices);
+      }
+
+      auto line = std::make_shared<glk::ThinLines>(candidate_line, true);
+
+      line->set_line_width(1.0f);
+      viewer->update_drawable("candidate_line", line, guik::FlatColor(1.0f, 1.0f, 1.0f, 0.5f));
+      is_candidate_key_frame_plot_ = false;
+    }
+
+    if (latest_sensor_points_ == nullptr) continue;
+    if (latest_sensor_points_->points.size() == 0) continue;
+    auto cloud_buffer = glk::create_point_cloud_buffer(*latest_sensor_points_);
+    viewer->update_drawable("sensor", cloud_buffer, guik::FlatColor(1.0, 1.0, 1.0, 0.2));
+
+    viewer->update_drawable(
+      "base_link", glk::Primitives::coordinate_system(),
+      guik::VertexColor(latest_pose_matrix_.matrix().cast<float>()));
+  }
+}
+
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE(LidarGraphSlamViewer)
